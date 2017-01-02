2ecaa9386113ce0dd1342b700590e9a7ad7b8dc8
Matias <matias.gimenez@rga.com>
2017-01-02 15:10:17 -0300

    improvements

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,24 +1,61 @@
-(function(doc, $, events, templates) {
+(function(win, doc, $, events, templates) {
   'use strict';
 
   var app = {
 
     init: function() {
       this.populateEvents();
+      this.zonesHandler();
     },
 
-    populateEvents: function() {
-      let zone = doc.location.search.split('?zone=')[1],
-          $nextEvents = $('.next-events').addClass('loading');
+    populateEvents: function(z) {
+      let location = doc.location.search,
+          zone = z || location.split('?zone=')[1],
+          $nextEvents = $('.js-next-events').addClass('loading'),
+          defaultZone = 'buenos-aires';
 
       events.getEvents(zone).then(function(events) {
         $nextEvents
           .append(templates.listEvents(events))
           .removeClass('loading');
       });
+
+      // first time
+      if (z === undefined) {
+        zone = zone === undefined ? defaultZone : zone;
+        location = location === '' ? '?zone=' + defaultZone : location;
+        $('a[href="' + location || + '"]').addClass('active');
+        win.history.pushState({'zone':zone},'eRaver', '/?zone=' + zone);
+      }
+
+
+    },
+
+    getZones: function(zone, self) {
+      $('.js-list-zones a').removeClass('active');
+      $(self).addClass('active');
+      $('.js-next-events').empty();
+      app.populateEvents(zone);
+
+    },
+
+    zonesHandler: function() {
+      $('.js-list-zones').on('click', 'a', function(ev) {
+        ev.preventDefault();
+        let zone = ev.target.href.split('?zone=')[1];
+        app.getZones(zone, this);
+        win.history.pushState({'zone':zone},'eRaver', '/?zone=' + zone);
+      });
+
+      window.onpopstate = function(event) {
+        if (event.state !== null) {
+          let trigger = $('a[href="?zone=' + event.state.zone + '"]');
+          app.getZones(event.state.zone, trigger);
+        }
+      };
     }
   };
 
   app.init();
 
-}(window.document, window.jQuery, window.events, window.templates));
+}(window, window.document, window.jQuery, window.events, window.templates));
